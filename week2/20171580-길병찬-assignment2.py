d8a2e09294882c98c9467b88eed2c47e2362e9e1
jjangga <jjangga@kookmin.ac.kr>
2017-09-10 13:06:54 +0900

    restructured directory

--- week2/20171580-길병찬-assignment2.py
+++ week2/20171580-길병찬-assignment2.py
@@ -7,24 +7,27 @@ def factorial(n):
         else functools.reduce(lambda former, latter:
                               former * latter, range(1, n + 1))
 
+
 while True:
-    n = input("An argument for factorial : ")
+    str_n = input("An argument for factorial : ")
 
     try:
         # 문자열이나 소수 입력시 Exception 이 발생하여 다시 input 을 받는다.
-        int_n = int(n)
+        int_n = int(str_n)
 
         # -1 미만의 정수 입력시 다시 input을 받는다.
         if int_n < -1:
             raise ValueError()
 
-        # -1 입력시 종료한다.
-        elif int_n == -1:
-            break
-
-        print(factorial(int_n))
-
     except ValueError:
         print("Invalid Argument. Please input again")
+        continue
+
+    # -1 입력시 종료한다.
+    if int_n == -1:
+        break
+
+    print("%d!  =  %d" % (int_n, factorial(int_n)))
 
 print("Terminated")
+
