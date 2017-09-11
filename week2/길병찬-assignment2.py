ce0e1191594d944f5d635707d3aad6b5ac753fec
jjangga <jjangga@kookmin.ac.kr>
2017-09-11 22:26:44 +0900

    logical REVERT to before review in order to show diff

--- week2/길병찬-assignment2.py
+++ week2/길병찬-assignment2.py
@@ -18,14 +18,14 @@ while True:
         # -1 미만의 정수 입력시 다시 input을 받는다.
         if int_n < -1:
             raise ValueError()
+        
+        # -1 입력시 종료한다.
+        if int_n == -1:
+            break
 
     except ValueError:
         print("Invalid Argument. Please input again")
         continue
-
-    # -1 입력시 종료한다.
-    if int_n == -1:
-        break
 
     print("%d!  =  %d" % (int_n, factorial(int_n)))
 
