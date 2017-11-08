59a9000d847f5e7ac1fb328a0f39aa3fe02d9cde
jjangga <jjangga@kookmin.ac.kr>
2017-11-08 23:52:50 +0900

    주석 추가 및 정리

--- week 10/mycalc3_copied.py
+++ week 10/mycalc3_copied.py
@@ -43,7 +43,7 @@ class Calculator(QWidget):
         # 버튼들에 들어갈 텍스트를 만든다. [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.', '=']
         # 참고 : itertools.chain 함수는 여러 iterable(반복 가능한 것들)들을 순서대로 연결해준다.
         buttonTexts = itertools.chain(range(7, 10), range(4, 7), range(1, 4), (0, '.', '='))
-        # 레이아웃에 버튼들을 한줄에 2개씩 배치한다.
+        # 레이아웃에 버튼들을 한줄에 3개씩 배치한다.
         alignButtonsOnGrid(numLayout, buttonTexts, unit=3, onClicked=self.buttonClicked)
         # Number Layout을 mainLayout의 1행 0열에 배치한다.
         mainLayout.addLayout(numLayout, 1, 0)
@@ -72,13 +72,21 @@ class Calculator(QWidget):
 # 버튼 GridLayout 에
 def alignButtonsOnGrid(layout: QGridLayout, texts, unit: int, onClicked: callable):
     '''
-    layout 위에 버튼들을 올려놓는 메서드이다.
+    layout의 왼쪽 위부터 시작해 오른쪽으로 버튼들을 배치하는 메서드이다.
+    즉, 한 줄에 unit 개의 버튼들을 배치하고, 줄이 꽉 차면 아랫줄로 이동해 배치한다.
+
     :param layout: 버튼들을 올려놓을 QGridLayout 객체
     :param texts: 버튼들을 생성하는 데 필요한 텍스트
     :param unit: 한 줄에 가능한 버튼의 수. 즉, layout의 열(column) 수
-    :param onClicked: Button 생성자에 인자로 주어질 callback
+    :param onClicked: Button 생성자에 인자로 주어질 callback [click 이벤트 핸들러]
     '''
     for i, text in enumerate(texts):
+        '''
+        때로, for문에 range()를 쓰지 않지만, 반복하면서 인덱스가 필요한 경우가 있다.
+        이럴때 enumerate 함수를 쓰면 인덱스까지 사용할수 있어서 좋다.
+        (유의 : 여기서 자료구조 안의 인덱스가 아니라 단순히 for 문이 몇번의 반복인지를 의미한다.)
+        이 경우에는 text는 texts 안의 요소들이고 i는 논리적인 인덱스이다.
+        '''
         button = Button(str(text), onClicked)
         layout.addWidget(button, i // unit, i % unit)
 
