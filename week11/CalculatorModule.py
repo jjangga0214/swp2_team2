3162b4312a7ee66696a949f99e51b76b259d6208
kimgyuri <rosa980309@kookmin.ac.kr>
2017-11-14 11:01:04 +0900

    add assignment7_김규리

---
+++ week11/CalculatorModule.py
@@ -0,0 +1,4 @@
+from calcFunctions import factorial, decToBin, binToDec, decToRoman
+
+cons = {'pi' : '3.141592', '빛의 이동 속도 (m/s)' : '3E+8', '소리의 이동 속도 (m/s)' : '340', '태양과의 평균 거리 (km)' : '1.5E+8'}
+func = {'factorial (!)' : factorial, '-> binary' : decToBin, 'binary -> dec' : binToDec, '-> roman' : decToRoman}
---
+++ week11/calcFunctions.py
@@ -0,0 +1,28 @@
+from math import factorial as fact
+
+def factorial(numStr):
+    try:
+        n = int(numStr)
+        r = str(fact(n))
+    except:
+        r = 'Error!'
+    return r
+
+def decToBin(numStr):
+    try:
+        n = int(numStr)
+        r = bin(n)[2:]
+    except:
+        r = 'Error!'
+    return r
+
+def binToDec(numStr):
+    try:
+        n = int(numStr, 2)
+        r = str(n)
+    except:
+        r = 'Error!'
+    return r
+
+def decToRoman(numStr):
+    return 'dec -> Roman'
